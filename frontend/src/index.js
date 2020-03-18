4325733baebd1c7d05b4346ae6ff2b603f269093
Bruno <bruno.webjr@gmail.com>
2020-03-17 21:13:49 -0300

    added apollo client

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ApolloClient, { gql } from 'apollo-boost';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000'
+});
+
+client.query({
+    query: gql`
+        {
+            books {
+                title
+                author
+            }
+        }
+    `
+}).then(result => console.log(result));
+
 ReactDOM.render(<App />, document.getElementById('root'));
