3738a37ff880813786f0e5980e40d946dcd72cea
Bruno <bruno.webjr@gmail.com>
2020-04-01 21:59:48 -0300

    deleted some files

--- controllers/book.ctrl.js
+++
@@ -1,7 +0,0 @@
-const Book = require('../models/book.model');
-
-module.exports = Book.find().then(books => (
-    books.map(book => {
-        return {title: book.title,author: book.author}
-    })
-));
