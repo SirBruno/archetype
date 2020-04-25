af54e42542393a46e32f958d1490743de474b20f
Bruno <bruno.webjr@gmail.com>
2020-04-25 20:38:04 -0300

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Books from './Components/Books.jsx';
+import Books from './Components/Books';
 
 class App extends Component {
 
