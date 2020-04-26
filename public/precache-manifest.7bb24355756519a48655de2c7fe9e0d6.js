300d6acfa4923b936727da1a99839bee8fc97855
Bruno <bruno.webjr@gmail.com>
2020-04-25 21:31:42 -0300

    ...

---
+++ public/precache-manifest.7bb24355756519a48655de2c7fe9e0d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08e68d5ab57198f26990e35023f47d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52f4a1ba2b15083fbdf4",
+    "url": "/static/js/2.b97aac2f.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/static/js/2.b97aac2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5cdf7d10513dc66ca6",
+    "url": "/static/js/main.03ec4465.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  }
+]);
