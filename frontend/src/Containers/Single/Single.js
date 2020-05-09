9fc95416b19df694f2e3e38655cc9bd111df9be4
Bruno <bruno.webjr@gmail.com>
2020-05-09 00:29:36 -0300

    posts can now be updated

--- frontend/src/Containers/Single/Single.js
+++ frontend/src/Containers/Single/Single.js
@@ -8,6 +8,10 @@ export default function Single() {
   const client = useApolloClient();
   const [book, setBook] = useState(0);
 
+  const dataTitle = React.createRef();
+  const dataAuthor = React.createRef();
+  const dataDescription = React.createRef();
+
   useEffect(() => {
 
     const getBook = async () => {
@@ -16,6 +20,7 @@ export default function Single() {
         query: gql`
           query book($_id: String){
             book(_id: $_id) {
+              id
               title
               author
               description
@@ -32,6 +37,34 @@ export default function Single() {
     getBook()
   });
 
+  const updateBook = async (_id) => {
+
+    console.log('*********************')
+    console.log(_id);
+    console.log(dataTitle.current.value);
+    console.log('*********************')
+    const res = await client.mutate({
+      variables: {
+        _id,
+        title: dataTitle.current.value,
+        author: dataAuthor.current.value,
+        description: dataDescription.current.value
+      },
+      mutation: gql`
+        mutation updateBook($_id: String, $title: String, $author: String, $description: String){
+          updateBook(_id: $_id, title: $title, author: $author, description: $description) {
+            id
+            title
+            author
+            description
+          }
+        }
+    `,
+    })
+
+    console.log(res);
+  }
+
   if (book === 0) {
     return <h3>Loading...</h3>
   } else return (
@@ -39,6 +72,13 @@ export default function Single() {
       <h3>{book.title}</h3>
       <p>{book.author}</p>
       <p>{book.description}</p>
+      <input ref={dataTitle} defaultValue={book.title} />
+      <br />
+      <input ref={dataAuthor} defaultValue={book.author} />
+      <br />
+      <input ref={dataDescription} defaultValue={book.description} />
+      <br />
+      <button onClick={() => updateBook(book.id)}>Submit</button>
     </div>
   )
 }
