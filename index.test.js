0b8eb3f31fbf7af9ae52e082373aec3e8165ee81
Bruno <bruno.webjr@gmail.com>
2020-07-20 23:02:10 -0300

    ...

--- index.test.js
+++ index.test.js
@@ -4,16 +4,6 @@ test('Check if a query is returning \'OK\'', () => {
   return axios({
     url: 'http://archetypeofficial.herokuapp.com/graphql',
     method: 'post',
-    data: {
-      query: `
-        query {
-          posts {
-            id
-          }
-        }
-        `
-    }
-  }).then(res => {
-    expect(res.status).toBe(200);
-  });
-});
+    data: { query: `query { posts { id } }` }
+  }).then(res => { expect(res.status).toBe(200) })
+})
