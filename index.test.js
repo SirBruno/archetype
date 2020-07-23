f4ba6d1be301a5cd6ce8aa9c505a7e5cb02a94c5
Bruno <bruno.webjr@gmail.com>
2020-07-22 23:34:31 -0300

    changed the graphql endpoint to use https instead

--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@ const axios = require("axios")
 
 test('Check if a query is returning \'OK\'', () => {
   return axios({
-    url: 'http://archetypeofficial.herokuapp.com/graphql',
+    url: 'https://archetypeofficial.herokuapp.com/graphql',
     method: 'post',
     data: { query: `query { posts { id } }` }
   }).then(res => { expect(res.status).toBe(200) })
