3f0bffcaa8c0a69be9c56cc09c753ab35549c71b
Bruno <bruno.webjr@gmail.com>
2020-08-10 23:17:35 -0300

    changes to the database and API

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,52 +1,310 @@
-const Post = require('../models/post.model')
+const Schemas = require('../models/Schemas')
 
 const resolvers = {
+  // ############################# POST #############################
   Query: {
-    posts: () => Post.find({}),
-    post: async (root, { _id }) => await Post.findById(_id)
+    posts: () => Schemas.Post.find({}),
+    post: async (root, { _id }) => await Schemas.Post.findById(_id),
   },
+  
+  // ############################# USER #############################
+  Query: {
+    users: () => Schemas.User.find({}),
+    user: async (root, { _id }) => await Schemas.User.findById(_id)
+  },
+
+  // ############################# REPORT #############################
+  Query: {
+    reports: () => Schemas.Report.find({}),
+    report: async (root, { _id }) => await Schemas.Report.findById(_id)
+  },
+
+  // ############################# COMMENT #############################
+  Query: {
+    comments: () => Schemas.Comment.find({}),
+    comment: async (root, { _id }) => await Schemas.Comment.findById(_id)
+  },
+
+  // ############################# CATEGORY #############################
+  Query: {
+    categories: () => Schemas.Category.find({}),
+    category: async (root, { _id }) => await Schemas.Category.findById(_id)
+  },
+
   Mutation: {
+    // ################################################################
+    // ############################# POST #############################
+    // ################################################################
+
     addPost: async (_, args) => {
       try {
-        let response = await Post.create({
+        let response = await Schemas.Post.create({
           postTitle: args.postTitle,
           author: args.author,
           postBody: args.postBody,
-          postLikes: 0
-        });
-        return response;
-      } catch (e) {
-        return e.message;
-      }
+          postLikes: 0,
+          userId: args.userId,
+          categoryId: args.categoryId,
+          postComments: args.postComments,
+          postStatus: args.postStatus,
+          postVisibility: args.postVisibility,
+          postImageURL: args.postImageURL,
+          postTags: args.postTags
+        })
+        return response
+      } catch (e) { return e.message }
     },
     updatePost: async (_, args) => {
       try {
         // make a call to Post.findById, so i can access the fields inside this block
         // i'm doing this to prevent a field saving as "null" if not specified in the args
         // otherwise, non-specified fields would make the existing respective field null
-        let postToUpdate = await Post.findById(args._id, 'postTitle author postBody postLikes').exec()
-        
-        let response = await Post.findOneAndUpdate({ _id: args._id },
+        let postToUpdate = await Schemas.Post.findById(
+          args._id,
+          `postTitle
+          author
+          postBody
+          postLikes
+          userId
+          categoryId
+          postComments
+          postStatus
+          postVisibility
+          postImageURL
+          postTags`
+        ).exec()
+
+        let response = await Schemas.Post.findOneAndUpdate({ _id: args._id },
           {
             $set: {
               // if the field was specified, update it the with new value
               // otherwise, keep the old value
-              postTitle: args.postTitle || postToUpdate.postTitle,
-              author: args.author || postToUpdate.author,
-              postBody: args.postBody || postToUpdate.postBody,
-              postLikes: args.postLikes || postToUpdate.postLikes
+              postTitle: args.postTitle == null ? postToUpdate.postTitle : args.postTitle,
+              author: args.author == null ? postToUpdate.author : args.author,
+              postBody: args.postBody == null ? postToUpdate.postBody : args.postBody,
+              postLikes: args.postLikes == null ? postToUpdate.postLikes : args.postLikes,
+              userId: args.userId == null ? postToUpdate.userId : args.userId,
+              categoryId: args.categoryId == null ? postToUpdate.categoryId : args.categoryId,
+              postComments: args.postComments == null ? postToUpdate.postComments : args.postComments,
+              postStatus: args.postStatus == null ? postToUpdate.postStatus : args.postStatus,
+              postVisibility: args.postVisibility == null ? postToUpdate.postVisibility : args.postVisibility,
+              postImageURL: args.postImageURL == null ? postToUpdate.postImageURL : args.postImageURL,
+              postTags: args.postTags == null ? postToUpdate.postTags : args.postTags
             }
-          }, { new: true });
-        return response;
+          }, { new: true })
+        return response
       } catch (e) { return e.message }
     },
     deletePost: async (_, args) => {
       try {
-        let response = await Post.findOneAndRemove(args)
-        return response;
+        let response = await Schemas.Post.findOneAndRemove(args)
+        return response
+      } catch (e) { return e.message }
+    }
+  },
+
+  Mutation: {
+    // ################################################################
+    // ############################# USER #############################
+    // ################################################################
+
+    addUser: async (_, args) => {
+      try {
+        let response = await Schemas.User.create({
+          userName: args.userName,
+          userPassword: args.userPassword,
+          userEmail: args.userEmail,
+          userExp: 0,
+          userLevel: 0,
+          userPermission: args.userPermission,
+          userRanking: args.userRanking,
+          userPosts: args.userPosts,
+          userComments: args.userPosts,
+          userReports: args.userReports,
+        })
+        console.log(response)
+        return response
+      } catch (e) { console.log(e.message); return e.message }
+    },
+    updateUser: async (_, args) => {
+      try {
+        let userToUpdate = await Schemas.User.findById(
+          args._id,
+          `userName
+          userPassword
+          userEmail
+          userExp
+          userLevel
+          userPermission
+          userRanking
+          userPosts
+          userComments
+          userReports`
+        ).exec()
+
+        let response = await Schemas.User.findOneAndUpdate({ _id: args._id },
+          {
+            $set: {
+              userName: args.userName == null ? userToUpdate.userName : args.userName,
+              userPassword: args.userPassword == null ? userToUpdate.userPassword : args.userPassword,
+              userEmail: args.userEmail == null ? userToUpdate.userEmail : args.userEmail,
+              userExp: args.userExp == null ? userToUpdate.userExp : args.userExp,
+              userLevel: args.userLevel == null ? userToUpdate.userLevel : args.userLevel,
+              userPermission: args.userPermission == null ? userToUpdate.userPermission : args.userPermission,
+              userRanking: args.userRanking == null ? userToUpdate.userRanking : args.userRanking,
+              userPosts: args.userPosts == null ? userToUpdate.userPosts : args.userPosts,
+              userComments: args.userComments == null ? userToUpdate.userComments : args.userComments,
+              userReports: args.userReports == null ? userToUpdate.userReports : args.userReports,
+            }
+          }, { new: true })
+        return response
+      } catch (e) { return e.message }
+    },
+    deleteUser: async (_, args) => {
+      try {
+        let response = await Schemas.User.findOneAndRemove(args)
+        return response
+      } catch (e) { return e.message }
+    }
+  },
+
+  Mutation: {
+    // ##################################################################
+    // ############################# REPORT #############################
+    // ##################################################################
+
+    addReport: async (_, args) => {
+      try {
+        let response = await Schemas.Report.create({
+          postId: args.postId,
+          commentId: args.commentId,
+          userId: args.userId,
+          reportTitle: args.reportTitle,
+          reportBody: args.reportBody
+        })
+        console.log(response)
+        return response
+      } catch (e) { console.log(e.message); return e.message }
+    },
+    updateReport: async (_, args) => {
+      try {
+        let reportToUpdate = await Schemas.Report.findById(
+          args._id,
+          `postId
+          commentId
+          userId
+          reportTitle
+          reportBody
+          `
+        ).exec()
+
+        let response = await Schemas.Report.findOneAndUpdate({ _id: args._id },
+          {
+            $set: {
+              postId: args.postId == null ? reportToUpdate.postId : args.postId,
+              commentId: args.commentId == null ? reportToUpdate.commentId : args.commentId,
+              userId: args.userId == null ? reportToUpdate.userId : args.userId,
+              reportTitle: args.reportTitle == null ? reportToUpdate.reportTitle : args.reportTitle,
+              reportBody: args.reportBody == null ? reportToUpdate.reportBody : args.reportBody
+            }
+          }, { new: true })
+        return response
+      } catch (e) { return e.message }
+    },
+    deleteReport: async (_, args) => {
+      try {
+        let response = await Schemas.Report.findOneAndRemove(args)
+        return response
+      } catch (e) { return e.message }
+    }
+  },
+
+  Mutation: {
+    // ###################################################################
+    // ############################# COMMENT #############################
+    // ###################################################################
+
+    addComment: async (_, args) => {
+      try {
+        let response = await Schemas.Comment.create({
+          userId: args.userId,
+          commentBody: args.commentBody,
+          commentLikes: 0,
+          commentReplies: args.commentReplies
+        })
+        return response
+      } catch (e) { return e.message }
+    },
+    updateComment: async (_, args) => {
+      try {
+        let commentToUpdate = await Schemas.Comment.findById(
+          args._id,
+          `userId
+          commentBody
+          commentLikes
+          commentReplies`
+        ).exec()
+
+        let response = await Schemas.Comment.findOneAndUpdate({ _id: args._id },
+          {
+            $set: {
+              userId: args.userId == null ? commentToUpdate.userId : args.userId,
+              commentBody: args.commentBody == null ? commentToUpdate.commentBody : args.commentBody,
+              commentLikes: args.commentLikes == null ? commentToUpdate.commentLikes : args.commentLikes,
+              commentReplies: args.commentReplies == null ? commentToUpdate.commentReplies : args.commentReplies
+            }
+          }, { new: true })
+        return response
+      } catch (e) { return e.message }
+    },
+    deleteComment: async (_, args) => {
+      try {
+        let response = await Schemas.Comment.findOneAndRemove(args)
+        return response
+      } catch (e) { return e.message }
+    }
+  },
+
+  Mutation: {
+    // ####################################################################
+    // ############################# CATEGORY #############################
+    // ####################################################################
+
+    addCategory: async (_, args) => {
+      try {
+        let response = await Schemas.Category.create({
+          categoryPosts: args.categoryPosts,
+          categoryTitle: args.categoryTitle
+        })
+        console.log(response)
+        return response
+      } catch (e) { console.log(e.message); return e.message }
+    },
+    updateCategory: async (_, args) => {
+      try {
+        let categoryToUpdate = await Schemas.Category.findById(
+          args._id,
+          `categoryPosts
+          categoryTitle`
+        ).exec()
+
+        let response = await Schemas.Category.findOneAndUpdate({ _id: args._id },
+          {
+            $set: {
+              categoryPosts: args.categoryPosts == null ? categoryToUpdate.categoryPosts : args.categoryPosts,
+              categoryTitle: args.categoryTitle == null ? categoryToUpdate.categoryTitle : args.categoryTitle
+            }
+          }, { new: true })
+        return response
+      } catch (e) { return e.message }
+    },
+    deleteCategory: async (_, args) => {
+      try {
+        let response = await Schemas.Category.findOneAndRemove(args)
+        return response
       } catch (e) { return e.message }
     }
   }
 }
 
-module.exports = resolvers;
+module.exports = resolvers
