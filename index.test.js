4a1ab0608817e08b4b0e40a7a9d8b252d2f7e2db
Bruno <bruno.webjr@gmail.com>
2020-08-27 23:00:53 -0300

    added pagination and fixed the test query

--- index.test.js
+++ index.test.js
@@ -4,6 +4,6 @@ test('Check if a query is returning \'OK\'', () => {
   return axios({
     url: 'http://localhost:4000/graphql',
     method: 'post',
-    data: { query: `query { posts { id } }` }
+    data: { query: `query { posts( pageSize: 10) { hasMore cursor posts { id } } }` }
   }).then(res => { expect(res.status).toBe(200) })
 })
